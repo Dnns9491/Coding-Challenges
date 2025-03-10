e6458138d7157b69ef6c70c61252fe2cf097067d
Dennis Sasi <dennis_1994@hotmail.co.uk>
2025-03-10 22:06:22 +0000

    adding new problem

---
+++ src/main/java/HackerRank/ifElseProblem.java
@@ -0,0 +1,46 @@
+package HackerRank;
+/*
+Task
+Given an integer, n, perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of 2 to 5, print Not Weird
+If  is even and in the inclusive range of 6 to 20, print Weird
+If  is even and greater than 20, print Not Weird
+
+Input format
+A single line containing a positive integer, n.
+
+Constraints
+1 <= n <= 100
+ */
+
+import java.util.Scanner;
+
+public class ifElseProblem {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if(N % 2 == 1){
+            System.out.println("Weird");
+        } else if (N>=2 && N <=5) {
+            System.out.println("Not Weird");
+        } else if (N>=6 && N <=20) {
+            System.out.println("Weird");
+        } else if (N > 20) {
+            System.out.println("Not weird");
+        } else {
+            System.out.println("undefined");
+        }
+    }
+}
+
+/*
+The reason we don't declare the scanner method within the main method, is because
+1. If another method in the class has to use Scanner, it can be reused.
+2. Avoids creating multiple Scanner objects unnecessarily.
+3. If declared inside the main method, it only exists in the main
+ */
