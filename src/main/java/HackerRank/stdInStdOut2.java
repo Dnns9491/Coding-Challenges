e70a33cc0c14f0499e2f49e111d786a68ed68a87
Dennis Sasi <dennis_1994@hotmail.co.uk>
2025-03-30 17:27:52 +0100

    adding new problem test for stdInStdOut

--- src/main/java/HackerRank/stdInStdOut2.java
+++ src/main/java/HackerRank/stdInStdOut2.java
@@ -18,12 +18,23 @@ On the second line, print Double: followed by the unaltered double read from std
 On the third line, print Int: followed by the unaltered integer read from stdin.
  */
 
-
-
-
+import java.util.Scanner;
 
 public class stdInStdOut2 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine(); // Consume the newline ch
+        scan.close();// aracter after the double input
+
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
 
     }
 }
+
+
